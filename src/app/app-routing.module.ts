86f1caaac5758845045e46d380f39d0c6427792b
Leonardo Lira Becerra <leonardolirabecerra@Leonardos-MacBook-Pro.local>
2018-08-31 17:25:49 -0500

    [NEW] Create and deploy on firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UserInterfaceComponent } from './_components/layout/user-interface/user-interface.component';
+import { StartComponent } from './_components/start/start.component';
+
+const routes: Routes = [
+    {
+        path: '', component: UserInterfaceComponent, children: [
+            { path: 'home', component: StartComponent },
+
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+        ]
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { UserInterfaceComponent } from './_components/layout/user-interface/user-interface.component';
+import { StartComponent } from './_components/start/start.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UserInterfaceComponent,
+        StartComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
