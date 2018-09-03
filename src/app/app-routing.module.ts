b0ab5bfdf024574f59703855cb133bb06a0f1c54
Leonardo Lira Becerra <leonardolirabecerra@Leonardos-MacBook-Pro.local>
2018-09-03 12:29:46 -0500

    [NEW] Page not found redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { UserInterfaceComponent } from './_components/layout/user-interface/user-interface.component';
 import { StartComponent } from './_components/start/start.component';
+import { PageNotFoundComponent } from './_components/layout/page-not-found/page-not-found.component';
 
 const routes: Routes = [
     {
         path: '', component: UserInterfaceComponent, children: [
             { path: 'home', component: StartComponent },
 
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: '404', component: PageNotFoundComponent },
+            { path: '', redirectTo: '404', pathMatch: 'full' },
         ]
     }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppComponent } from './app.component';
 import { UserInterfaceComponent } from './_components/layout/user-interface/user-interface.component';
 import { StartComponent } from './_components/start/start.component';
+import { PageNotFoundComponent } from './_components/layout/page-not-found/page-not-found.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         UserInterfaceComponent,
-        StartComponent
+        StartComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
+        MaterializeModule,
         AppRoutingModule
     ],
     providers: [],
