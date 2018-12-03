9fd00e53bada9b9e95bf4dbc73eab59622192517
Leonardo Lira Becerra <leonardolirabecerra@Leonardos-MacBook-Pro.local>
2018-12-02 22:02:15 -0600

    solving errors

--- src/app/_services/payment.service.ts
+++ src/app/_services/payment.service.ts
@@ -1,13 +1,8 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {environment} from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class PaymentService {
-
-    constructor(
-        private http: HttpClient
-    ) {}
+    constructor() {}
 }
